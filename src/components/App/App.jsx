1728ae564945ac8b6e2797686d9400203a6c7b29
Eben Stine <ebenstine@gmail.com>
2021-08-19 21:50:58 -0500

    created components, created routes in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import Feeling from '../Feeling/Feeling';
+import Understanding from '../Understanding/Understanding';
+import Supported from '../Supported/Supported';
+import Comments from '../Comments/Comments';
+import Review from '../Review/Review';
+
 
 function App() {
 
   return (
     <div className='App'>
+      <Router>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Route path="/" exact>
+        <Feeling />
+      </Route>
+
+      <Route path="/understanding" exact>
+        <Understanding />
+      </Route>
+
+      <Route path="/supported" exact>
+        <Supported />
+      </Route>
+
+      <Route path="/review" exact>
+        <Review />
+      </Route>
+
+      <Route path="/comments" exact>
+        <Comments />
+      </Route>
+    </Router>
     </div>
   );
 }
