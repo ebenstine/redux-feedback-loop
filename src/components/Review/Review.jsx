1c6d7ce391c3df97a427965ff3e38ca15329fb65
Eben Stine <ebenstine@gmail.com>
2021-08-21 19:06:14 -0500

    readd review component

---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+
+function Review() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const feedback = useSelector(store => store.allFeedback)
+    
+
+
+    const submitFeedback = () => {
+        if (feedback.length > 4){
+            alert ('Cannot revisit given answers')
+            dispatch({
+            type: 'SUBMIT_RESET',
+        });
+        history.push("/")
+        } else {
+        axios({
+            type:'POST',
+            url: '/feedback',
+            data: {
+                feeling: feedback[0],
+                understanding: feedback[1],
+                support: feedback[2],
+                comments: feedback[3]
+            }
+        }).then((response) => {
+            console.log(response.data);
+            console.log('feedback submitted');
+        }).catch((error) =>{
+            console.log('BIGTIME POST error', error);
+        });
+        history.push('/submitted');
+        }
+    }
+    return (
+        <>
+        <h2>Your Self-Reflection for Today:</h2>
+        <h3>Feelings: {feedback[0]}</h3>
+        <h3>Understanding: {feedback[1]}</h3>
+        <h3>Support: {feedback[2]}</h3>
+        <h3>Comments: {feedback[3]}</h3>
+        <button onClick={submitFeedback}>Submit</button>
+        
+        </>
+    )
+
+}
+
+export default Review;
